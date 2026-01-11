aeb6df31b1fd2142413c5c019869035d3a5582ae
cod lucas <100681227+COD-LUCAS@users.noreply.github.com>
2026-01-11 20:02:00 +0530

    Delete plugins directory

--- plugins/ping.js
+++
@@ -1,9 +0,0 @@
-export const command = "ping";
-
-export async function run({ sock, msg }) {
-  const start = Date.now();
-  await sock.sendMessage(msg.key.remoteJid, { text: "Pinging..." });
-  await sock.sendMessage(msg.key.remoteJid, {
-    text: `Ping: ${Date.now() - start} ms`
-  });
-}
