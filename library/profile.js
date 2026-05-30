f38a48a3e380d32ea350f16ef718dfa0e20edc27
cod lucas <100681227+COD-LUCAS@users.noreply.github.com>
2026-05-30 11:27:22 +0530

    Update profile.js

--- library/profile.js
+++ library/profile.js
@@ -1,45 +1,38 @@
-const Jimp = require("jimp");
+/**
+ * library/profile.js
+ * Helper to update profile picture in full size without square crop.
+ */
+
+'use strict';
+
+const Jimp = require('jimp');
 
 async function generateProfilePicture(buffer) {
-  try {
-    const jimp = await Jimp.read(buffer);
-    const min = jimp.bitmap.width;
-    const max = jimp.bitmap.height;
-    const cropped = jimp.crop(0, 0, min, max);
-    return {
-      img: await cropped.scaleToFit(324, 720).getBufferAsync(Jimp.MIME_JPEG),
-      preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG)
-    };
-  } catch (err) {
-    console.error("Generate profile error:", err);
-    throw err;
-  }
+  const jimp = await Jimp.read(buffer);
+  const min  = jimp.getWidth();
+  const max  = jimp.getHeight();
+  const cropped = jimp.crop(0, 0, min, max);
+  return {
+    img:     await cropped.scaleToFit(324, 720).getBufferAsync(Jimp.MIME_JPEG),
+    preview: await cropped.normalize().getBufferAsync(Jimp.MIME_JPEG)
+  };
 }
 
-async function updatefullpp(jid, imag, client) {
-  try {
-    const { query } = client;
-    const { img } = await generateProfilePicture(imag);
-    await query({
-      tag: "iq",
-      attrs: {
-        to: jid,
-        type: "set",
-        xmlns: "w:profile:picture"
-      },
-      content: [{
-        tag: "picture",
-        attrs: {
-          type: "image"
-        },
-        content: img
-      }]
-    });
-    return true;
-  } catch (err) {
-    console.error("Update PP error:", err);
-    return false;
-  }
+async function updatefullpp(jid, buffer, sock) {
+  const { img } = await generateProfilePicture(buffer);
+  await sock.query({
+    tag: 'iq',
+    attrs: {
+      to:    '@s.whatsapp.net',
+      type:  'set',
+      xmlns: 'w:profile:picture'
+    },
+    content: [{
+      tag:     'picture',
+      attrs:   { type: 'image' },
+      content: img
+    }]
+  });
 }
 
-module.exports = { updatefullpp, generateProfilePicture };
+module.exports = { updatefullpp };
